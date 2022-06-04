1563f9ff73ae950b1660ef92629f83fa2af5354c
Behrouz Talavari <b.talavari@persici.co.uk>
2022-06-04 12:01:12 +0430

    Add To do Item

---
+++ Client/Client/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Models
+{
+    public class ToDoItem
+    {
+        public int ItemId { get; set; }
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Client/Client/Pages/CodeSeperation.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Pages
+{
+    public partial class CodeSeperation
+    {
+        public string Message { get; set; } = "This is Code Seperation Sample!";
+    }
+}
